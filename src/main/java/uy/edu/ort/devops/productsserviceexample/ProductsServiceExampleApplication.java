84e8d328eb2093da52e9bc34da7d62bb53dc971f
javieryeduardo <108472477+javieryeduardo@users.noreply.github.com>
2022-07-19 15:30:19 -0300

    Update ProductsServiceExampleApplication.java

--- src/main/java/uy/edu/ort/devops/productsserviceexample/ProductsServiceExampleApplication.java
+++ src/main/java/uy/edu/ort/devops/productsserviceexample/ProductsServiceExampleApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductsServiceExampleApplication {
 
 
-	public statiion.run(ProductsServiceExampleApplication.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsServiceExampleApplication.class, args);
 	}
 
+
 }
+
+
